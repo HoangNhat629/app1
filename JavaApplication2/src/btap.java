39be09a5e57bfe67b84fd6f6365aa6b3a1a5a5eb
unknown <ngocnhathht@gmail.com>
2019-08-14 21:06:19 +0700

    java1

---
+++ JavaApplication2/src/btap.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class btap {
+    public static void main(String[] args) {
+        double aa , bb , cc ;
+        double result;
+        System.out.println("Nhap vao a:");
+        aa= sc.nextDouble();
+        System.out.println("Nhap vao b:");
+        bb = sc.nextDouble();
+        System.out.println("Nhap vao c:");
+        result = (cc-bb)/aa;
+        System.out.println("ket qua la:"+result);
+        
+    }
+
+    }
+          
+            
+    
+}
---
+++ JavaApplication2/src/java1.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class java1 {
+    //giai phuong trinh bac nhat ax+b=0;
+    public static void main(String[] args) {
+     int a=7;
+     int b=3;
+     float x;
+     x= -b/a;
+     // /%
+     // + - * / %
+     // int/float=>float
+     //int/int=> chia lay phan nguyen
+    
+     int sodu=7%3;
+     System.out.println(sodu);
+     Scanner sc = new Scanner(System.in);
+     
+     
+        
+        
+        
+        
+        
+    }
+}
+
