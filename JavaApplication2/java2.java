eda4a02618346374be6c4a2ce49cf365ce463e3e
unknown <ngocnhathht@gmail.com>
2019-08-18 00:50:13 +0700

    baitap

---
+++ JavaApplication2/java2.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class java2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        String input;
+        System.out.println("nhap vao:");
+        //a = sc.nextInt();// nhap vao so nguyen
+        input = sc.nextLine();
+     System.out.println("gia tri a vua nhap vao la:"+ input);// sout -
+             //ax + b=0
+             int q = 3;
+             float p =  5.029f;
+             String w = "nhat";
+             String o = "00P";
+             System.out.printf("p = %.2f \nq = %d\na",p,q);//%d decimal int, %s  string
+             // \n la xuong dong;
+             //%d la dinh dang kieu du lieu in ra;
+             
+             
+            }
+}
